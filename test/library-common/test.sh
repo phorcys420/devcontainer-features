c69b8f364054ca39257f989ad3c58bb12776a903
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2024-07-10 14:53:33 +0000

    fix(library-common): hardcode library folder

--- test/library-common/test.sh
+++ test/library-common/test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-check "Library folder exists" test -d "$BASEDIR/common"
+check "Library folder exists" test -d "/usr/share/phorcys-devcontainer-libraries/common"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
