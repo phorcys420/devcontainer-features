638a3fa28246a91e0266838de1407feeb9b1366c
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-07-12 14:58:54 +0200

    feat: move features to libraries (#14)
    * feat: move features to libraries
    
    * chore: migrate features to use library features

--- src/burp-suite/install.sh
+++ src/burp-suite/install.sh
@@ -1,33 +1,46 @@
 #!/usr/bin/env bash
 
-echo "Activating feature 'burp-suite'"
-
 set -euo pipefail
 
+FEATURE_NAME="burp-suite"
+echo "Activating feature '$FEATURE_NAME'"
+
+# Source lib-common feature
+source "/usr/share/phorcys-devcontainer-libraries/common/1/main.sh"
+
+# Check for dependencies
+checkPackages curl ca-certificates jq sudo
+
+# Load options
 EDITION=${VERSION:-community}
 VERSION=${VERSION:-latest}
 
 if [ $VERSION = "latest" ]; then
-  echo "[burp-suite] [+] Grabbing the latest Burp version"
+  echo "[$FEATURE_NAME] [+] Grabbing the latest Burp version"
 
-  RELEASE_DATA=$(curl 'https://portswigger.net/burp/releases/data?previousLastId=-1&lastId=-1&pageSize=1' --silent --show-error)
-  VERSION=$(echo -n "$RELEASE_DATA" | jq -r '.ResultSet.Results[0].version')
+  RELEASE_DATA=$(curl 'https://portswigger.net/burp/releases/data?previousLastId=-1&lastId=-1&pageSize=5' --silent --show-error --fail)
+
+  # Get the first release with channel "Stable"
+  VERSION=$(echo -n "$RELEASE_DATA" | jq -r '.ResultSet.Results | map(select(.releaseChannels[] | contains ("Stable"))) | .[0].version')
 fi
 
 TMP=$(mktemp -d)
 DESTINATION_FILE="$TMP/burp_install.sh"
 
-echo "[burp-suite] [+] Downloading version $CURRENT_VERSION"
-curl --location --silent --show-error \
+echo "[$FEATURE_NAME] [+] Downloading version $VERSION $EDITION"
+
+curl --get --location --silent --show-error --fail \
   --data-urlencode "product=$EDITION" \
-  --data-urlencode "version=$CURRENT_VERSION" \
+  --data-urlencode "version=$VERSION" \
   --data-urlencode "type=Linux" \
   --output "$DESTINATION_FILE" \
   "https://portswigger-cdn.net/burp/releases/download"
 
-chmod +x "$DESTINATION_FILE"
+# Make temporary directory accessible to all users
+chmod +rx "$TMP" -R
 
-echo "[burp-suite] [+] Installing Burp CE"
+echo "[$FEATURE_NAME] [+] Installing Burp CE"
+_REMOTE_USER="coder"
 sudo -u "$_REMOTE_USER" "$DESTINATION_FILE" -q # $_REMOTE_USER_HOME
 
 rm -rf "$TMP"
--- src/ghidra/install.sh
+++ src/ghidra/install.sh
@@ -1,49 +1,39 @@
 #!/usr/bin/env bash
 
-echo "Activating feature 'ghidra'"
-
 set -euo pipefail
 
+FEATURE_NAME="ghidra"
+echo "Activating feature '$FEATURE_NAME'"
+
+LIBRARY_FOLDER="/usr/share/phorcys-devcontainer-libraries"
+
+# Source lib-common and lib-github features
+source "$LIBRARY_FOLDER/common/1/main.sh"
+source "$LIBRARY_FOLDER/github/1/main.sh"
+
+# Load options
 REPOSITORY=${REPOSITORY:-NationalSecurityAgency/ghidra}
 VERSION=${VERSION:-latest}
 
 INSTALL_DIR=${INSTALL_DIR:-/opt/ghidra}
 
-# From aws-cli feature (https://github.com/devcontainers/features/blob/main/src/aws-cli/install.sh#L59-L72)
-export DEBIAN_FRONTEND=noninteractive
-check_packages() {
-  if ! dpkg -s "$@" > /dev/null 2>&1; then
-      if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
-          echo "Running apt-get update..."
-          apt-get update -y
-      fi
+# Check for dependencies
+checkPackages curl ca-certificates jq libarchive-tools
 
-      apt-get install -y --no-install-recommends "$@"
-  fi
-}
-
-check_packages curl ca-certificates jq libarchive-tools
-
-if [ $VERSION = "latest" ]; then
-    RELEASE_API_URL="https://api.github.com/repos/NationalSecurityAgency/ghidra/releases/latest"
-else
-    RELEASE_API_URL="https://api.github.com/repos/NationalSecurityAgency/ghidra/releases/tags/$VERSION"
-fi
-
-ASSET_URL=$(curl "$RELEASE_API_URL" --silent --show-error | jq -r ".assets[0].browser_download_url")
+ASSET_URL=$(getRelease "$REPOSITORY" "$VERSION" | jq -r ".assets[0].browser_download_url")
 
 TMP=$(mktemp -d)
 DESTINATION_FILE="$TMP/ghidra.zip"
 
-echo "[ghidra] [+] Downloading version with tag $VERSION"
+echo "[$FEATURE_NAME] [+] Downloading version with tag $VERSION"
 
-curl --location --silent --show-error \
-  --output "$DESTINATION_FILE" \
-  "$ASSET_URL"
+curl --get --location --silent --show-error --fail \
+    --output "$DESTINATION_FILE" \
+    "$ASSET_URL"
 
 mkdir -p "$INSTALL_DIR"
 
-echo "[ghidra] [+] Extracting to $INSTALL_DIR"
+echo "[$FEATURE_NAME] [+] Extracting to $INSTALL_DIR"
 
 # Extract the archive by stripping the first directory so that we don't end up with a subfolder (e.g ghidra_11.1.1_PUBLIC_20240614) in $INSTALL_DIR 
 bsdtar --strip-components=1 -xf "$DESTINATION_FILE" -C "$INSTALL_DIR"
