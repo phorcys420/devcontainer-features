7eb769186b3b5c98113ce5dadb7e92b5ca39ca71
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2024-07-12 12:00:53 +0000

    feat: move features to libraries

--- src/burp-suite/install.sh
+++ src/burp-suite/install.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-echo "Activating feature 'burp-suite'"
-
 set -euo pipefail
 
+FEATURE_NAME="burp-suite"
+echo "Activating feature '$FEATURE_NAME'"
+
+# Source lib-common feature
+source "/usr/share/phorcys-devcontainer-libraries/common/1/main.sh"
+
+# Check for dependencies
+checkPackages curl ca-certificates jq sudo
+
+# Load options
 EDITION=${VERSION:-community}
 VERSION=${VERSION:-latest}
 
 if [ $VERSION = "latest" ]; then
-  echo "[burp-suite] [+] Grabbing the latest Burp version"
+  echo "[$FEATURE_NAME] [+] Grabbing the latest Burp version"
 
   RELEASE_DATA=$(curl 'https://portswigger.net/burp/releases/data?previousLastId=-1&lastId=-1&pageSize=1' --silent --show-error)
   VERSION=$(echo -n "$RELEASE_DATA" | jq -r '.ResultSet.Results[0].version')
@@ -17,7 +25,7 @@ fi
 TMP=$(mktemp -d)
 DESTINATION_FILE="$TMP/burp_install.sh"
 
-echo "[burp-suite] [+] Downloading version $CURRENT_VERSION"
+echo "[$FEATURE_NAME] [+] Downloading version $CURRENT_VERSION"
 curl --location --silent --show-error \
   --data-urlencode "product=$EDITION" \
   --data-urlencode "version=$CURRENT_VERSION" \
@@ -27,7 +35,7 @@ curl --location --silent --show-error \
 
 chmod +x "$DESTINATION_FILE"
 
-echo "[burp-suite] [+] Installing Burp CE"
+echo "[$FEATURE_NAME] [+] Installing Burp CE"
 sudo -u "$_REMOTE_USER" "$DESTINATION_FILE" -q # $_REMOTE_USER_HOME
 
 rm -rf "$TMP"
