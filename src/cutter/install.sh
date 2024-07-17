4b3d722e4cecce938096291586f11bab0bcb30ff
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-07-17 12:13:19 +0200

    feat(cutter): add option to extract appimage contents (#22)

--- src/cutter/install.sh
+++ src/cutter/install.sh
@@ -15,20 +15,43 @@ source "$LIBRARY_FOLDER/github/1/main.sh"
 REPOSITORY=${REPOSITORY:-rizinorg/cutter}
 VERSION=${VERSION:-latest}
 
+EXTRACT_APPIMAGE=${EXTRACTAPPIMAGE:-"true"}
+
 INSTALL_DIR=${INSTALL_DIR:-/opt/cutter}
-BINARY_PATH="$INSTALL_DIR/cutter"
 
 # Check for dependencies
 checkPackages curl ca-certificates jq libarchive-tools
 
 ASSET_URL=$(getRelease "$REPOSITORY" "$VERSION" | jq -r '.assets[] | select(.name | endswith("Linux-x86_64.AppImage")) | .browser_download_url')
 
+TMP=$(mktemp -d)
+DESTINATION_FILE="$TMP/cutter"
+
 echo "[$FEATURE_NAME] [+] Downloading version with tag $VERSION"
+
+curl --get --location --silent --show-error --fail \
+    --output "$DESTINATION_FILE" \
+    "$ASSET_URL"
+
+chmod +x "$DESTINATION_FILE"
 
 mkdir -p "$INSTALL_DIR"
 
-curl --get --location --silent --show-error --fail \
-    --output "$BINARY_PATH" \
-    "$ASSET_URL"
+if [ "$EXTRACT_APPIMAGE" = "true" ]; then
+    pushd "$TMP"
 
-chmod +x "$BINARY_PATH"
+    echo "[$FEATURE_NAME] [+] Extracting AppImage"
+    "$DESTINATION_FILE" --appimage-extract
+
+    shopt -s dotglob nullglob
+
+    # When extracting the AppImage, it creates a squashfs-root subdirectory
+    mv squashfs-root/* "$INSTALL_DIR/"
+
+    # Rename the "AppRun" binary to "cutter"
+    mv "$INSTALL_DIR/AppRun" "$INSTALL_DIR/cutter"
+
+    popd
+else
+    mv "$DESTINATION_FILE" "$INSTALL_DIR/"
+fi
