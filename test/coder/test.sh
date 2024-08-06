ebd5cd894fa11a15eac13ccc1fbf09b54a9aafdc
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2024-08-06 18:53:16 +0200

    feat: add `coder` feature

---
+++ test/coder/test.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Optional: Import test library bundled with the devcontainer CLI
+source dev-container-features-test-lib
+
+check "Coder CLI exists" "which coder"
+
+# Report results
+# If any of the checks above exited with a non-zero exit code, the test will fail.
+reportResults
