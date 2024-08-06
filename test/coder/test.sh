abdbe0abac4bd469562c9be73a0ebec2c92f7e03
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2024-08-06 18:59:03 +0200

    fix(test/coder): use `command` instead of `which`

--- test/coder/test.sh
+++ test/coder/test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-check "Coder CLI exists" "which coder"
+check "Coder CLI exists" "command -v coder &> /dev/null"
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
