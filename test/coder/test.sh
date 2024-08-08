4c52b5f5a9efc9c1bc2c9c0eaa131022d0da3f3f
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2024-08-08 12:04:27 +0000

    fix: install Coder system-wide instead of per user

--- test/coder/test.sh
+++ test/coder/test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-# Do nothing
+check "Coder binary is in the PATH" which coder
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
