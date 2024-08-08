c67b83c1cccf5968e345effa0014c04cab955526
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2024-08-08 11:46:52 +0000

    feat: add software to PATH when installing it

--- test/cutter/test.sh
+++ test/cutter/test.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-check "Cutter folder exists" test -d /opt/cutter
+check "Cutter folder exists" test -d "$CUTTER_HOME"
+check "Cutter binary is in the PATH" which cutter
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
