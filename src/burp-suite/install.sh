21ddcec55f6c51006a8292a2d61d50cfd77c82fb
Phorcys <57866459+phorcys420@users.noreply.github.com>
2024-08-11 22:14:20 +0200

    fix: fix burp tests and add `$BURP_HOME` (#32)
    * fix: fix burp tests and add `$BURP_HOME`
    
    * chore: "Burp" -> "Burp Suite"

--- src/burp-suite/install.sh
+++ src/burp-suite/install.sh
@@ -8,15 +8,18 @@ echo "Activating feature '$FEATURE_NAME'"
 # Source lib-common feature
 source "/usr/share/phorcys-devcontainer-libraries/common/1/main.sh"
 
-# Check for dependencies
-checkPackages curl ca-certificates jq sudo
+# Check for dependencies (libfreetype6 is only really needed to run in CI, otherwise you would always have this library if you have the desktop-lite feature)
+checkPackages curl ca-certificates jq sudo libfreetype6
 
 # Load options
 EDITION=${EDITION:-community}
 VERSION=${VERSION:-latest}
 
+# CUTTER_HOME is defined in the containerEnv value of the feature's manifest
+BURP_HOME=${BURP_HOME:-/opt/burp-suite}
+
 if [ $VERSION = "latest" ]; then
-  echo "[$FEATURE_NAME] [+] Grabbing the latest Burp version"
+  echo "[$FEATURE_NAME] [+] Grabbing the latest Burp Suite version"
 
   RELEASE_DATA=$(curl 'https://portswigger.net/burp/releases/data?previousLastId=-1&lastId=-1&pageSize=5' --silent --show-error --fail)
 
@@ -39,7 +42,7 @@ curl --get --location --silent --show-error --fail \
 # Make temporary directory accessible to all users
 chmod +rx "$TMP" -R
 
-echo "[$FEATURE_NAME] [+] Installing Burp $EDITION"
-sudo -u "$_REMOTE_USER" "$DESTINATION_FILE" -q # $_REMOTE_USER_HOME
+echo "[$FEATURE_NAME] [+] Installing Burp Suite $EDITION edition"
+"$DESTINATION_FILE" -q -dir "$BURP_HOME" -overwrite
 
 rm -rf "$TMP"
--- test/burp-suite/test.sh
+++ test/burp-suite/test.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
-# Do nothing
+check "Burp folder exists" test -d "$BURP_HOME"
+check "Burp binary is in the PATH" which BurpSuiteCommunity
 
 # Report results
 # If any of the checks above exited with a non-zero exit code, the test will fail.
